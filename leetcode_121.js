a3c2cced119e0fdded6e5abf16a5c96165b0b464
MouthWashGuy <brandonyongwork@gmail.com>
2023-07-21 01:11:32 +0800

    Completed leetcode_121 in javascript
    did it in python first, honestly dont know why this one took me so long. Feel kind of dumb considering the solution is quite simple.

--- leetcode_121.js
+++ leetcode_121.js
@@ -5,11 +5,36 @@
 // helper functions
 
 // test inputs
-prices = [7,1,5,3,6,4]
-prices = [7,6,4,3,1]
+let prices1 = [7,1,5,3,6,4];
+let prices2 = [7,6,4,3,1];
 
 // solution here
 // important notes: this is an array representing the prices of a single stock, the price fluctuates day by day, but the crux of it, is we must buy it on a certain day and be able to sell it for more in the future.
+// key idea, we can keep track of the lowest price seen as we iterate over the price list, when we see a lower price than the one before, we begin checking against that one instead
+// at the end we return the highest profit seen from all comparisons
+
+function solution(prices) {
+
+    let max_profit = 0;
+    let lowest_price = prices[0];
+
+    // now the for loop
+    for (let price of prices) {
+
+        // if the ith price is < than the previously seen lowest, dont bother comparing, just switch em
+        if (price < lowest_price) {
+            lowest_price = price;
+        } else {
+            max_profit = Math.max(max_profit, price - lowest_price);
+        }
+    }
+
+    // now return the max profit seen
+    return max_profit;
+
+}
 
 
 // testing
+console.log(solution(prices1));
+console.log(solution(prices2));
--- sandbox.py
+++ sandbox.py
@@ -1,1 +1,33 @@
+prices = [7,1,5,3,6,4]
+
+def sol(prices):
+
+# we can do it in O(n) time and O(1) space using a simple two pointer approach
+        # the trick is to designate 2 pointers, one for the index for the day we wish to buy the stock
+        # another for the index we wish to sell
+        # when we find that the sell < buy pointers prices, we will shift both pointers to that location and continue checking from there
+
+        if len(prices) <= 1:
+            return 0
+
+        buy_pointer = 0 # we try and buy on the first day
+        sell_pointer = 1 # and then compare it to day 2
+
+        max_profit = 0 # of course if we find that we keep running into negative profit, we can just return 0 at the end
+
+        # heres the loop
+        while(sell_pointer < len(prices)):
+
+            # here we can do the shift if we notice that sell_pointer < buy pointer
+            if prices[buy_pointer] < prices[sell_pointer]:
+                buy_pointer = sell_pointer
+                sell_pointer = buy_pointer + 1
+
+            # now we can begin incrementing and price checks
+            max_profit = max(max_profit, prices[sell_pointer] -  prices[buy_pointer])
+            sell_pointer += 1
+
+        return max_profit
+
+print(sol(prices))
 
